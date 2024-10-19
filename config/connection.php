c1770a1bb5377a444b8bc05e6bc417572e5943ae
Sumanth <gsumanth101@gmail.com>
2024-10-19 22:22:32 +0530

    i

--- config/connection.php
+++ config/connection.php
@@ -1,29 +1,15 @@
 <?php
-require __DIR__ . '/../vendor/autoload.php';
+$servername = "localhost";
+$username = "eyebook_user";
+$password = "#**Admin123@**#";
+$dbname = "eyebook";
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
-
-$servername = $_ENV['DB_SERVERNAME'];
-$username = $_ENV['DB_USERNAME'];
-$password = $_ENV['DB_PASSWORD'];
-$dbname = $_ENV['DB_NAME'];
-
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-// Create database if it doesn't exist
-$sql = "CREATE DATABASE IF NOT EXISTS $dbname";
-if ($conn->query($sql) !== TRUE) {
-    die("Error creating database: " . $conn->error);
-}
-
-// Select the database
-$conn->select_db($dbname);
 
 // Function to close the connection
 function closeConnection($conn) {
