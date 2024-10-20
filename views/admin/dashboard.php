0f6601d6010d8db6cea98a70f58c052aef828d3b
Sumanth <gsumanth101@gmail.com>
2024-10-20 07:53:43 +0530

    i

--- views/admin/dashboard.php
+++ views/admin/dashboard.php
@@ -89,11 +89,11 @@ $conn->close();
             </div>
         </div>
         <div class="row">
-            <div class="col-md-6 grid-margin stretch-card">
+            <!-- <div class="col-md-6 grid-margin stretch-card">
                 <div class="card tale-bg">
                     <div class="card-people mt-auto">
                         <img src="images/dashboard/people.svg" alt="people">
-                        <!-- <div class="weather-info">
+                        <div class="weather-info">
                     <div class="d-flex">
                       <div>
                         <h2 class="mb-0 font-weight-normal"><i class="icon-sun mr-2"></i>31<sup>C</sup></h2>
@@ -103,10 +103,10 @@ $conn->close();
                         <h6 class="font-weight-normal">India</h6>
                       </div>
                     </div>
-                  </div> -->
+                  </div>
                     </div>
                 </div>
-            </div>
+            </div> -->
             <div class="col-md-6 grid-margin transparent">
                 <div class="row">
                     <div class="col-md-6 mb-4 stretch-card transparent">
